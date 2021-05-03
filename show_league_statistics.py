9be967ea7cd9b3d428a63316010c19612dca602c
Maciej Szczypek <s20092@pjwstk.edu.pl>
2021-05-03 16:19:07 +0200

    Remove timing functions.

--- show_league_statistics.py
+++ show_league_statistics.py
@@ -1,7 +1,8 @@
-import tabulate
 import argparse
+
+import tabulate
+
 from modules.league_statistics_provider import BasicLeagueStatisticsProvider
-import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,9 +14,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     league_statistics_provider = BasicLeagueStatisticsProvider(league_entry=args.league_entry)
-    t0 = time.time()
     teams_statistics = league_statistics_provider.get_league_statistics()
-    t1 = time.time()
     tabulated_team_statistics = tabulate.tabulate(teams_statistics, headers="keys")
     print(tabulated_team_statistics)
-    print(t1 - t0)
